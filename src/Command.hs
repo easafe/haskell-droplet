ebf37dc583c78864d5af36dd0060e2aea227007a
Eduardo Asafe <e@asafe.dev>
2021-11-17 01:09:58 +0800

    Tests for define

--- src/Command.hs
+++ src/Command.hs
@@ -1,17 +1,38 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
 
-module Command (Options (..), define, makeTables, print) where
+module Command where
 
 import Constants (
+    autoType,
+    booleanType,
+    closeBracket,
+    comma,
+    dash,
     dateImport,
     dateTimeImport,
+    dateTimeType,
+    dateType,
     defaultImportList,
+    defaultType,
     disclaimer,
+    doubleColon,
+    equals,
+    ident,
+    intType,
     moduleWhere,
     newLine,
+    nullableType,
+    numberType,
+    openBracket,
+    proxyType,
+    questionMark,
+    quote,
+    stringType,
+    tableType,
+    typeKeyword,
  )
 import qualified Data.Foldable as DF
 import Data.HashMap.Strict ((!))
@@ -20,6 +41,7 @@ import Data.Hashable (Hashable)
 import qualified Data.List as DL
 import qualified Data.Maybe as DM
 import Data.Text (Text, empty)
+import qualified Data.Text as DS
 import qualified Data.Text as DT
 import qualified Data.Text.Encoding as DTE
 import qualified Database.PostgreSQL.Simple as DPS
@@ -44,19 +66,24 @@ data Options = Options
 -- | A table marks the definition and the column row type
 data Table = Table
     { originalName :: Text
+    , camelCaseName :: Text
     , columns :: [Column]
     }
+    deriving (Show, Eq)
 
 -- | Row Type information
 data Column = Column
     { originalName :: Text
+    , camelCaseName :: Text
     , dataType :: ColumnType
     }
+    deriving (Show, Eq)
 
 data ColumnType = ColumnType
     { wrapper :: Wrapper
     , typed :: Typed
     }
+    deriving (Show, Eq)
 
 data Wrapper
     = Auto
@@ -65,6 +92,7 @@ data Wrapper
     | Default
     | -- | No wrapper for this type
       None
+    deriving (Show, Eq)
 
 data Typed
     = String
@@ -75,7 +103,7 @@ data Typed
     | Boolean
     | -- | Outputted as a typed hole
       Unknown
-    deriving (Eq, Generic)
+    deriving (Show, Eq, Generic)
 
 instance Hashable Typed
 
@@ -88,52 +116,83 @@ define Options{connectionUrl, input, schema, moduleBaseName, definitionsFolder} 
 
 makeTables :: [ColumnDefinition] -> [Table]
 makeTables columns = map toTable grouped
-  where
-    grouped = DL.groupBy (\c d -> table_name c == table_name d) columns
+    where
+        grouped = DL.groupBy (\c d -> table_name c == table_name d) columns
 
-    toTable definitions =
-        let original = table_name $ head definitions
-         in Table
-                { originalName = original
-                , columns = map toColumn definitions
+        toTable definitions =
+            let original = table_name $ head definitions
+             in Table
+                    { originalName = original
+                    , camelCaseName = toCamelCase original
+                    , columns = map toColumn definitions
+                    }
+
+        toColumn def@ColumnDefinition{column_name} =
+            Column
+                { originalName = column_name
+                , camelCaseName = toCamelCase column_name
+                , dataType = toColumnType def
                 }
 
-    toColumn def@ColumnDefinition{column_name} =
-        Column
-            { originalName = column_name
-            , dataType = toColumnType def
-            }
+        toColumnType ColumnDefinition{data_type, is_nullable, is_identity, column_default} =
+            let wrapper
+                    | is_nullable = Nullable
+                    | is_identity = Auto
+                    | DM.isJust column_default = Default
+                    | otherwise = None
+                typed
+                    | data_type == "text" || DL.isPrefixOf "char" data_type = String
+                    | data_type == "integer" || data_type == "smallint" || data_type == "bigint" = Int
+                    | DL.isPrefixOf "numeric" data_type || data_type == "decimal" || data_type == "real" || DL.isPrefixOf "double" data_type = Number
+                    | data_type == "date" = Date
+                    | DL.isPrefixOf "timestamp" data_type = DateTime
+                    | data_type == "boolean" = Boolean
+                    | otherwise = Unknown
+             in ColumnType{wrapper = wrapper, typed = typed}
 
-    toColumnType ColumnDefinition{data_type, is_nullable, is_identity, column_default} =
-        let wrapper
-                | is_nullable = Nullable
-                | is_identity = Auto
-                | DM.isJust column_default = Default
-                | otherwise = None
-            typed
-                | data_type == "text" || DL.isPrefixOf "char" data_type = String
-                | data_type == "integer" || data_type == "smallint" || data_type == "bigint" = Int
-                | DL.isPrefixOf "numeric" data_type || data_type == "decimal" || data_type == "real" || DL.isPrefixOf "double" data_type = Number
-                | data_type == "date" = Date
-                | DL.isPrefixOf "timestamp" data_type = DateTime
-                | data_type == "boolean" = Boolean
-                | otherwise = Unknown
-         in ColumnType{wrapper = wrapper, typed = typed}
+        toCamelCase = DT.pack . TC.camel . DT.unpack
 
 print :: Text -> Table -> Text
-print moduleBaseName Table{originalName, columns} = header
-  where
-    header = disclaimer <> moduleBaseName <> DT.toTitle (toCamelCase originalName) <> moduleWhere <> defaultImportList <> extraImports <> newLine
+print moduleBaseName Table{originalName, camelCaseName, columns} = DT.intercalate newLine [header, rowType, table, proxies]
+    where
+        header = disclaimer <> moduleBaseName <> titleName <> moduleWhere <> defaultImportList <> extraImports
 
-    extraImports =
-        let dataTypes = map (typed . dataType) columns
-            moduleDataType = DHS.fromList [(Date, dateImport), (DateTime, dateTimeImport)]
-            include running dt
-                | DL.elem dt dataTypes = running <> moduleDataType ! dt
-                | otherwise = running
-         in DL.foldl include empty [Date, DateTime]
+        rowType = typeKeyword <> titleName <> equals <> newLine <> ident <> openBracket <> DS.intercalate fieldSeparator (map toField columns) <> closeBracket <> newLine
 
-    toCamelCase = DT.pack . TC.camel . DT.unpack
+        table = camelCaseName <> doubleColon <> quote <> originalName <> quote <> newLine <> camelCaseName <> equals <> tableType
+
+        proxies = DT.intercalate newLine $ map toProxy columns
+
+        extraImports =
+            let dataTypes = map (typed . dataType) columns
+                moduleDataType = DHS.fromList [(Date, dateImport), (DateTime, dateTimeImport)]
+                include running dt
+                    | DL.elem dt dataTypes = running <> moduleDataType ! dt
+                    | otherwise = running
+             in DL.foldl include empty [Date, DateTime]
+
+        toField Column{originalName, camelCaseName, dataType = ColumnType{wrapper, typed}} =
+            let w =
+                    case wrapper of
+                        Auto -> autoType
+                        Nullable -> nullableType
+                        Default -> defaultType
+                        None -> empty
+                t = case typed of
+                    String -> stringType
+                    Int -> intType
+                    Number -> numberType
+                    Date -> dateType
+                    DateTime -> dateTimeType
+                    Boolean -> booleanType
+                    Unknown -> questionMark <> camelCaseName
+             in camelCaseName <> doubleColon <> w <> t
+
+        toProxy Column{originalName, camelCaseName} = dash <> camelCaseName <> doubleColon <> proxyType <> quote <> originalName <> quote <> newLine <> camelCaseName <> equals <> proxyType
+
+        titleName = DT.toTitle camelCaseName
+
+        fieldSeparator = comma <> newLine <> ident
 
 saveFile :: FilePath -> Text -> IO ()
 saveFile folderName fileContent = pure () -- do
--- src/Constants.hs
+++ src/Constants.hs
@@ -32,3 +32,66 @@ newLine = "\n"
 
 disclaimer :: Text
 disclaimer = "-- AUTO GENERATED CODE\n\n"
+
+typeKeyword :: Text
+typeKeyword = "type "
+
+equals :: Text
+equals = " = "
+
+openBracket :: Text
+openBracket = "( "
+
+doubleColon :: Text
+doubleColon = " :: "
+
+comma :: Text
+comma = ", "
+
+closeBracket :: Text
+closeBracket = ")"
+
+ident :: Text
+ident = "    "
+
+autoType :: Text
+autoType = "Auto "
+
+nullableType :: Text
+nullableType = "Maybe "
+
+defaultType :: Text
+defaultType = "Default "
+
+questionMark :: Text
+questionMark = "?"
+
+stringType :: Text
+stringType = "String"
+
+intType :: Text
+intType = "Int"
+
+numberType :: Text
+numberType = "Number"
+
+dateType :: Text
+dateType = "Date"
+
+dateTimeType :: Text
+dateTimeType = "DateTime"
+
+booleanType :: Text
+booleanType = "Boolean"
+
+tableType :: Text
+tableType = "Table "
+
+quote :: Text
+quote = "\""
+
+dash :: Text
+dash = "_"
+
+proxyType :: Text
+proxyType = "Proxy "
